364fcbe1bcdfd212af9a27ab799e57ddd9318747
Darya <d.byk@list.ru>
2022-03-04 17:09:06 +0300

    JS для продуктов

--- main.js
+++ main.js
@@ -6,7 +6,38 @@ let initProducts = () => {
     // console.log(shirts);
 
     // Your Code Here
-    console.log(shirts);
+    // shirts.length
+    let table = document.getElementsByClassName("products-table");
+    for(let i = 0; i < shirts.length; i++) {
+
+        //это для внешнего блока с картинкой
+        let divShirts = document.createElement('div');
+        divShirts.className = "products-table-block";
+        divShirts.innerHTML = '<img src = ' + shirts[i].colors.white.front + '>'
+        + '<h3>' + shirts[i].name + '</h3>'
+        + '<p>Available in ' + shirts[i].colors.length + '</p>'; //так не работает, потом поменять
+        //divShirts.insertAdjacentHTML("beforeend", '<p>Привет</p>');
+        document
+        .body
+        .children[0]
+        .children[1]
+        .children[1]
+        .insertAdjacentElement("beforeend", divShirts);
+    
+        //добавить еще блок с кнопками
+        let divButton = document.createElement('div');
+        divButton.className = "products-table-buttons";
+        divButton.innerHTML = '<button>Quick View</button>'
+        + '<button>Quick View</button>';
+        
+        document
+        .body
+        .children[0]
+        .children[1]
+        .children[1]
+        .children[i]
+        .insertAdjacentElement("beforeend", divButton);
+    }
 };
 
 let initDetails = () => {
