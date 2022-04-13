d49b079d00b90d7a02776d54aac462fdeced3590
dora-mandragora <d.byk@list.ru>
2022-04-13 19:24:25 +0300

    MouseDown

--- INIS-lab2/input.js
+++ INIS-lab2/input.js
@@ -6,46 +6,76 @@ let coordY;
 let elSize = dragEl.length;
 
 let isDoubleClick = false;
-
+let isMouseMove = false;
 
 for (let i=0; i<elSize; i++){ 
 
     let activeEl = dragEl[i];
 
-    activeEl.draggable= true;
-    //может сделать не через draggable    
-    activeEl.addEventListener('dragstart',(e) => { 
-        coordX = e.offsetX; 
-        coordY = e.offsetY; 
-    }); 
-    activeEl.addEventListener('dragend', (e) => { 
-        activeEl.style.top = (e.pageY - coordY) + 'px'; 
-        activeEl.style.left = (e.pageX - coordX) + 'px'; 
-    }); 
+    //activeEl.draggable= true;
+    ////может сделать не через draggable    
+    // activeEl.addEventListener('dragstart',(e) => { 
+    //     coordX = e.offsetX; 
+    //     coordY = e.offsetY; 
+    // }); 
+    // activeEl.addEventListener('dragend', (e) => { 
+    //     activeEl.style.top = (e.pageY - coordY) + 'px'; 
+    //     activeEl.style.left = (e.pageX - coordX) + 'px'; 
+    // }); 
 
-    
+        
     activeEl.addEventListener('click',onClick);    
     function onClick()
     { 
-        if(!isDoubleClick){                
-            activeEl.style.background = 'blue'; 
-            for (let j = 0; j< elSize; j++){ 
-                if (j!==i)                                  
-                    dragEl[j].style.background = 'red'; 
-            } 
+        if(!isDoubleClick){
+                            
+                activeEl.style.background = 'blue'; 
+                for (let j = 0; j< elSize; j++){ 
+                    if (j!==i)                                  
+                        dragEl[j].style.background = 'red'; 
+                  
+            }
+           
         }
     };  
     
+    activeEl.addEventListener('mousedown', onMouseDownDrag);
+    function onMouseDownDrag(e)
+    {                      
+        
+        let shiftX = e.clientX - activeEl.getBoundingClientRect().left;
+        let shiftY = e.clientY - activeEl.getBoundingClientRect().top;
+
+        function moveAt(pageX, pageY){
+            activeEl.style.top = pageY - shiftY + 'px'; 
+            activeEl.style.left = pageX - shiftX + 'px';
+        }
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }        
+        function onClickDragExit(){
+            activeEl.removeEventListener('mouseup',onClickDragExit);
+            document.removeEventListener('mousemove',onMouseMove, false);            
+            
+        }
+
+        document.addEventListener('mousemove', onMouseMove, false);        
+        activeEl.addEventListener('mouseup', onClickDragExit);
+    };
+
 
     activeEl.addEventListener('dblclick', onDblClickDrag);    
-    function onDblClickDrag()
+    function onDblClickDrag(e)
     {
         isDoubleClick = true;
         activeEl.style.background = 'yellow';
         
+        let shiftX = e.clientX - activeEl.getBoundingClientRect().left;
+        let shiftY = e.clientY - activeEl.getBoundingClientRect().top;
+
         function moveAt(pageX, pageY){
-            activeEl.style.top = pageY - activeEl.offsetHeight/2 + 'px'; 
-            activeEl.style.left = pageX - activeEl.offsetWidth/2 + 'px';
+            activeEl.style.top = pageY - shiftY + 'px'; 
+            activeEl.style.left = pageX - shiftX + 'px';
         }
         function onMouseMove(event) {
             moveAt(event.pageX, event.pageY);
